b62bdbcba1cd43e95e649868b3a3c094cd7dddb6
icenuts <billzeng808@gmail.com>
2014-04-07 20:55:15 -0500

    L1, L2 Cache;

---
+++ Simulator.java
@@ -0,0 +1,175 @@
+import java.lang.String;
+import java.util.zip.GZIPInputStream;
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Simulator
+{    
+
+    public String testTrace = "sjeng-1K"; // default
+
+    // cache stuff
+    public CacheStats L1_CacheStats = null;
+    public CacheStats L2_CacheStats = null;
+
+    public Cache L1_Cache = null;
+    public Cache L2_Cache = null;
+
+    // print options, by default off
+    public boolean verbose_f = false;
+    public boolean debug_f = false;
+
+    // optionally limit the simulation to the first N insns
+    // do not break this functionality when you complete the code!
+    public boolean insnLimit_f = false;
+    public int uopLimit = 0;
+	
+    // don't touch! you'll be sorry....
+    public BufferedReader traceReader = null;
+	
+    // Program Stats
+    public long totalUops = 0;
+	
+	// Customized Variables
+	public int storeCount = 0;
+	public int totalMemRef = 0;
+    
+    /*
+     * Simple constructor for the simulator.
+     */
+    public Simulator() {
+	L1_CacheStats = new CacheStats();
+	L2_CacheStats = new CacheStats();
+
+	storeCount = 0;
+    }
+
+    /*
+     * set up the trace
+     */
+    public void initTrace() throws IOException {
+	String tracePath = "traces/"+testTrace+".trace.gz";
+	// ugly, but JAVA IO is not the point
+	traceReader = new BufferedReader(new InputStreamReader(new GZIPInputStream(new FileInputStream(tracePath))));
+    }
+
+    /*
+     * Goes through the trace line by line (i.e., instruction by instruction) and 
+     * simulates the program being executed on a processor.
+     * 
+     * This method is currently stripped down. It will just read in each instruction 
+     * one at a time, and does not make any predictions. 
+     */
+    public void processTrace() throws IOException  {
+        String line = "";    
+        Uop currUop = null;
+        
+		initTrace();
+        
+        while (true) {
+	    line = traceReader.readLine();
+	    if (line == null) {
+		break;
+	    }
+	    if (insnLimit_f && totalUops == uopLimit) {
+		System.out.format("Reached insn limit of %d. Ending Simulation...\n", uopLimit);
+		break;
+	    }
+	    currUop = new Uop(line);
+	    totalUops++;
+	    
+	    // BEGIN WHERE YOU SHOULD MAKE CHANGES TO THIS FILE
+	    
+	    // Print this before you access the cache
+		
+		if(currUop.type == Uop.UopType.insn_LOAD || currUop.type == Uop.UopType.insn_STORE)
+		{
+			totalMemRef++;
+		    // access the cache
+		    // update the cache stats
+		    long []wb_tag = new long[1];
+		    // Clear flag
+		    wb_tag[0] = -1;
+		    
+		    boolean L1_hit_f = L1_Cache.access(currUop.addressForMemoryOp, wb_tag, currUop.type == Uop.UopType.insn_LOAD);
+		    L1_CacheStats.updateStat(L1_hit_f);
+
+		    // L1 Miss
+		    if (L1_hit_f == false)
+		    {
+		    	long []wb_tag2 = new long[1];
+		    	// Do something? L2 VictimBuffer? Miss Cache?
+		    	if (currUop.type == Uop.UopType.insn_LOAD) {
+		    		// Clear flag
+		    		wb_tag2[0] = -1;
+		    		boolean L2_hit_f =  L2_Cache.access(currUop.addressForMemoryOp, wb_tag2, true);
+		    		L2_CacheStats.updateStat(L2_hit_f);
+		    	}
+		    	// we need to write back
+				if (wb_tag[0] != -1) {
+					boolean L2_hit_f = L2_Cache.access(wb_tag[0], wb_tag2, false);
+					L2_CacheStats.updateStat(L2_hit_f);
+				}
+		    }
+
+			
+		    // Print this after you access the cache
+		    if (debug_f) 
+			System.out.format("%s\t%s\t%s\t%s\n", 
+					  Long.toHexString(L1_Cache.getBlockAddress(currUop.addressForMemoryOp)),
+					  currUop.isLoad() ? "L" : "S", 
+					  L1_hit_f ? "hit" : "miss", 
+					  L1_hit_f ? "--" : L1_Cache.dirtyEvic_f ? "dirty" : "clean");
+			if(currUop.type == Uop.UopType.insn_STORE)
+				storeCount++;
+		}
+
+	    // END WHERE YOU SHOULD MAKE CHANGES TO THIS FILE
+	    
+            // prints the insn
+	    if (verbose_f)
+		System.out.format("%s\n", line);
+	    
+        }
+		
+    	L1_CacheStats.calculateRates();
+		L1_CacheStats.total_bytes_transferred_wt = 4*storeCount + (int)L1_CacheStats.totalMisses()*L1_Cache.blockSize;
+		L1_CacheStats.total_bytes_transferred_wb = L1_Cache.dirtyCount * L1_Cache.blockSize + (int)L1_CacheStats.totalMisses()*L1_Cache.blockSize;
+		
+		L2_CacheStats.calculateRates();
+		L2_CacheStats.total_bytes_transferred_wt = 4*storeCount + (int)L2_CacheStats.totalMisses()*L2_Cache.blockSize;
+		L2_CacheStats.total_bytes_transferred_wb = L2_Cache.dirtyCount * L2_Cache.blockSize + (int)L2_CacheStats.totalMisses()*L2_Cache.blockSize;
+    }
+    
+    
+    /*
+     * Prints out basic stats + the accuracy of the branch predictor
+     */
+    public void printTraceStats() {
+	
+    	System.out.format("Processed %d trace lines.\n", totalUops);
+    	System.out.format("Num Uops (micro-ops): \t\t\t%d\n", totalUops);
+			
+		System.out.println("--------------L1 Stats--------------");
+		L1_CacheStats.print(); 
+		System.out.println("--------------L2 Stats--------------");
+        L2_CacheStats.print(); 
+	
+    }
+
+    public void printHeader() {
+	if (!debug_f) {
+	    System.out.format("HW 4 Printout for i-forgot-to-replace-this-string-with-my-wustlkey\n");
+	    System.out.format("-----------------------------------------------------\n");
+	}
+    	System.out.format("Trace name  \t\t\t\t%s\n", testTrace);
+    System.out.println("--------------L1 INFO--------------");
+	L1_Cache.printConfig();
+	System.out.println("--------------L2 INFO--------------");
+	L2_Cache.printConfig();
+	System.out.format("Instruction Limit\t\t\t%s\n", insnLimit_f ? Integer.toString(uopLimit) : "none");	
+    }
+    
+}
---
+++ cache.sh
@@ -0,0 +1,19 @@
+java HW4 -t sjeng-10M -L1 8192 4 1 -L2 262144 4 1 > out/way-1
+java HW4 -t sjeng-10M -L1 16384 8 1 -L2 262144 8 1 >> out/way-1
+java HW4 -t sjeng-10M -L1 32768 16 1 -L2 262144 16 1 >> out/way-1
+java HW4 -t sjeng-10M -L1 65536 32 1 -L2 262144 32 1 >> out/way-1
+
+java HW4 -t sjeng-10M -L1 8192 4 2 -L2 262144 4 2 > out/way-2
+java HW4 -t sjeng-10M -L1 16384 8 2 -L2 262144 8 2 >> out/way-2
+java HW4 -t sjeng-10M -L1 32768 16 2 -L2 262144 16 2 >> out/way-2
+java HW4 -t sjeng-10M -L1 65536 32 2 -L2 262144 32 2 >> out/way-2
+
+java HW4 -t sjeng-10M -L1 8192 4 4 -L2 262144 4 4 > out/way-4
+java HW4 -t sjeng-10M -L1 16384 8 4 -L2 262144 8 4 >> out/way-4
+java HW4 -t sjeng-10M -L1 32768 16 4 -L2 262144 16 4 >> out/way-4
+java HW4 -t sjeng-10M -L1 65536 32 4 -L2 262144 32 4 >> out/way-4
+
+java HW4 -t sjeng-10M -L1 8192 4 8 -L2 262144 4 8 > out/way-8
+java HW4 -t sjeng-10M -L1 16384 8 8 -L2 262144 8 8 >> out/way-8
+java HW4 -t sjeng-10M -L1 32768 16 8 -L2 262144 16 8 >> out/way-8
+java HW4 -t sjeng-10M -L1 65536 32 8 -L2 262144 32 8 >> out/way-8
